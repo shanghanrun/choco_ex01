c0ebdf1d63306da5432f03177a19d55f9b6adf61
shanghanrun <idim7@naver.com>
2023-11-23 12:14:44 +0900

    ListTile은 leading에서 제약이 있다. 그래서 커스텀위젯으로 바꾸었다.

--- lib/page_one.dart
+++ lib/page_one.dart
@@ -26,21 +26,61 @@ class _PageOneState extends State<PageOne> {
         ],
       ),
       body: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromARGB(255, 236, 242, 252).withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 12,
+              offset: const Offset(3, 3),
+            ),
+          ],
+        ),
         child: Center(
           child: ListView.builder(
+              itemExtent: 120,
               itemCount: displayingList.length,
               itemBuilder: (context, i) {
                 Map<String, dynamic> word = displayingList[i];
+                String imageName = word['image1'];
+                String wordName = word['word'];
+                String meanings = word['meanings'];
                 return Card(
-                  child: ListTile(
-                    leading: loadImage(word),
-                    title: Text(
-                      '  ${word['word']}',
-                      style: const TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text('   ' + word['meanings']),
-                    onTap: () {},
+                  elevation: 5,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(width: 10),
+                      SizedBox(
+                        width: 110,
+                        height: 80,
+                        child: loadImage(imageName),
+                      ),
+                      const SizedBox(width: 30), // 이미지와 텍스트 사이 간격 조정
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              wordName,
+                              style: const TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 5), // 텍스트 사이 간격 조정
+                            Text(
+                              meanings,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Color.fromARGB(255, 50, 60, 80),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 );
               }),
@@ -49,7 +89,7 @@ class _PageOneState extends State<PageOne> {
     );
   }
 
-  Widget loadImage(Map word) {
+  Widget loadImage(String imageName) {
     return Container(
       decoration: BoxDecoration(
         boxShadow: [
@@ -61,8 +101,7 @@ class _PageOneState extends State<PageOne> {
           ),
         ],
       ),
-      child:
-          Image.asset(word['image1'], height: 80, width: 70, fit: BoxFit.fill),
+      child: Image.asset(imageName, height: 80, width: 110, fit: BoxFit.fill),
     );
   }
 }
